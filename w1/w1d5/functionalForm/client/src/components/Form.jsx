134c954e7db5f9c9e13db43397a4851934d39a25
rubenocasio <108745971+rubenocasio@users.noreply.github.com>
2023-02-10 16:58:05 -0500

    Functional Components updates

--- w1/w1d5/functionalForm/client/src/components/Form.jsx
+++ w1/w1d5/functionalForm/client/src/components/Form.jsx
@@ -1,6 +1,15 @@
 //rafce
 import React, {useState} from 'react'
 
+const darkMode = {
+    backgroundColor: "black",
+    color: "white",
+}
+const lightMode = {
+    backgroundColor: "white",
+    color: "black",
+}
+
 const Form = () => {
     //State variables
     //Getters & Setters
@@ -8,6 +17,9 @@ const Form = () => {
     const [type, setType] = useState("")
     const [age, setAge] = useState("")
     const [img, setImg] = useState("")
+
+    const [light, setLight] = useState(true)
+    const switchLight = () => setLight(!light)
 
     const [listOfPets, setListOfPets] = useState([])
 
@@ -21,6 +33,16 @@ const Form = () => {
         setListOfPets([...listOfPets, petObj])
     }
 
+    const deletePet = (e, idx) => {
+        console.log("Deleting pet: ", idx)
+
+        let copy = listOfPets.filter((pet, i) => {
+            console.log(`${pet} ${idx} will be removed from our list of pets`)
+            return i !==idx
+        })
+        setListOfPets(copy)
+    }
+
     return (
         <div>
             <h1>Pet Form</h1>
@@ -28,14 +50,23 @@ const Form = () => {
                 <div>
                 <label>Pet Name:</label>
                 <input type="text" onChange={(e) => {setName(e.target.value)}} className="form-control"/>
+                {
+                    name.length < 3 && name.length > 0  ? <p className='text-danger'>Must fill out a name & be 3 characters long</p> : null
+                }
                 </div>
                 <div>
                 <label>Pet Type:</label>
                 <input type="text" onChange={(e) => {setType(e.target.value)}} className="form-control"/>
+                                {
+                    type.length < 3 && type.length > 0  ? <p className='text-danger'>Must fill out a name & be 3 characters long</p> : null
+                }
                 </div>
                 <div>
                 <label>Pet Age:</label>
                 <input type="text" onChange={(e) => {setAge(e.target.value)}} className="form-control"/>
+                {/* {
+                    age.length < 3 && age.length > 0  ? <p className='text-danger'>Must fill out a name & be 3 characters long</p> : null
+                } */}
                 </div>
                 <div>
                 <label>Pet Image:</label>
@@ -45,15 +76,20 @@ const Form = () => {
                     <button type="submit" className='btn btn-outline-dark'>Add Pet</button>
                 </div>
             </form>
-            <div className='pet-list'>
+            <div className='pet-list' style={light ? lightMode : darkMode}>
+                <button onClick={switchLight} className="btn btn-warning">{light ? "Dark Mode" : "Light Mode"}</button>
                 {
                     listOfPets.map((pet, idx) => {
                         return(
-                            <div key={idx}>
+                            <div key={idx} >
+                                <h2>This is my unique key: {idx}</h2>
                                 <h1>{pet.name}</h1>
                                 <h3>{pet.type}</h3>
                                 <h4>{pet.age}</h4>
                                 <img src={pet.img} alt="Pet Pic" height="150px"/>
+                                <br />
+                                <button onClick={(e) => deletePet(e, idx)} className="btn btn-outline-danger">Delete</button>
+                                <hr />
                             </div>
                         )
                     })
