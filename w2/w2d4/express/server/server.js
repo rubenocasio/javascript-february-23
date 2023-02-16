93185d8394c23bb046bee1c851bf5cf01fb42be5
rubenocasio <108745971+rubenocasio@users.noreply.github.com>
2023-02-16 10:39:35 -0500

    Create server.js

---
+++ w2/w2d4/express/server/server.js
@@ -0,0 +1,25 @@
+//import express and store express in a variable
+
+//initialize the express application and store it in a variable called 'app'
+
+//intialize the port to 8000
+
+
+//allow the application to parse json data (form information)
+//allow the application to accept form information
+
+
+
+//hardcoded data for the api (for now, until we get a database)
+const menuItems = [
+    {name: "Calamari", price: 12.00},
+    {name: "Maine Lobster Roll", price: 15.00},
+    {name: "Lasagna", price: 20},
+    {name: "Pizza", price: 10},
+    {name: "A5 Japanese Wagyu", price: 99},
+    { name: "🍔", price: 12.0 },
+    { name: "🍕", price: 15.0 },
+    { name: "🦑", price: 19.0 },
+    { name: "🍲", price: 9.0 },
+    { name: "🍝", price: 20.0 },
+]
