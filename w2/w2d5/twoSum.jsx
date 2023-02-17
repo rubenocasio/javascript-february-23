7fb1031afbdb9c737903a83a59acf9a69123ff1b
rubenocasio <108745971+rubenocasio@users.noreply.github.com>
2023-02-17 10:57:25 -0500

    Update twoSum.jsx

--- w2/w2d5/twoSum.jsx
+++ w2/w2d5/twoSum.jsx
@@ -3,7 +3,6 @@
 /*
   Given an array of integers, return indices of the two numbers such that they add up to a specific target.
   You may assume that each input would have exactly one solution, and you may not use the same element twice.
-  Bonus: Make it O(n) time
 */
 
 const nums1 = [2, 11, 7, 15];
